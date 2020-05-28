7e2f1adf60136ee403cd09d5bbafa3949e5f02c5
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-05-28 22:32:53 +0100

    created the 'new' and 'create' actions.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,5 +3,20 @@ class JobsController < ApplicationController
   end
 
   def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:title, :company, :url)
   end
 end
